98179c9d99008cd32a929dbcf9bb81fd14b180b9
Andres Betancurt <andbet050197@gmail.com>
2020-05-14 10:30:42 -0500

    Pagina Test terminada, Contacto, pagina acerca aún incompleta

--- landign_page/src/components/Contact.js
+++ landign_page/src/components/Contact.js
@@ -1,11 +1,50 @@
 import React from 'react';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faPhone, faMapMarkerAlt} from '@fortawesome/free-solid-svg-icons'
+import Map from "../images/Map.png"
+import "../css/Contact.css"
 
 function Contact() {
 
   return (   
     <div>
-        Contact
+        <React.Fragment>
+
+          <div className="row MyContainer">
+            <div className="col-6">
+              <img src={Map}></img>
+            </div>
+            <div id="Where" className="col-6">
+              <h1 className="title">¿DÓNDE TRABAJAMOS?</h1>
+              <p className="MyContent">
+                GENO-COL a expandido su acción en Colombia a través de convenios
+                locales creando una gran red de apoyo, y programas de diagnóstico
+                exitosos por todo el país. Con procesos meticulosos y bien establecidos
+                en gran parte del territorio, la investigación genética del cáncer ha roto
+                las barreras y ha sido cada vez mas accesible para los pacientes.
+              </p>
+            </div>
+          </div>
+
+          <div class="row text-center">
+            <div class="col-4">
+              <FontAwesomeIcon icon={faMapMarkerAlt} className="icon"/>
+              <p>CRA. 6 #72-34 ED. IMAT</p>
+              <p class="mb-0">MONTERIA - COLOMBIA</p>
+            </div>
+            <div class="col-4">
+              <FontAwesomeIcon icon={faPhone} className="icon"/>
+              <p>+57 310 6061877</p>
+              <p class="mb-0">+57 320 5744230</p>
+              <p class="mb-0">+57 313 3444492</p>
+            </div>
+            <div class="col-4">
+              <FontAwesomeIcon icon={faEnvelope} className="icon"/>
+              <p>genocolsas@gmail.com</p>
+              <p class="mb-0">comercialgenocol@gmail.com</p>
+            </div>
+          </div>
+        </React.Fragment>
     </div>
   );
 }
--- landign_page/src/components/Test.js
+++ landign_page/src/components/Test.js
@@ -1,14 +1,24 @@
 import React from 'react';
-
+import Step1 from "../images/TestKit.png"
+import Step2 from "../images/TestStep2.png"
+import Step3 from "../images/TestStep3.png"
+import Step4 from "../images/TestStep4.png"
+import Step5 from "../images/TestStep5.png"
+import "../css/Test.css"
 
 function Test() {
 
   return (   
     <div>
         <React.Fragment>
+          <div className="row justify-content-md-center">
+            <h1 className="title">
+              ¿CÓMO LO HACEMOS?
+            </h1>
+          </div>
           <div className="row MyContainer">
             <div className="col-6">
-              <img src=".."></img>
+              <img src={Step1}></img>
             </div>
             <div className="col-6">
               <h1 className="title">1. TOMA DE MUESTRAS Y ENVÍO DE KITS</h1>
@@ -33,12 +43,12 @@ function Test() {
               </p>
             </div>
             <div className="col-6">
-              <img src=".."></img>
+              <img src={Step2}></img>
             </div>
           </div>
           <div className="row MyContainer">
             <div className="col-6">
-              <img src=".."></img>
+              <img src={Step3}></img>
             </div>
             <div className="col-6">
               <h1 className="title">3. PROCESAMIENTO</h1>
@@ -59,12 +69,12 @@ function Test() {
               </p>
             </div>
             <div className="col-6">
-              <img src=".."></img>
+              <img src={Step4}></img>
             </div>
           </div>
           <div className="row MyContainer">
             <div className="col-6">
-              <img src=".."></img>
+              <img src={Step5}></img>
             </div>
             <div className="col-6">
               <h1 className="title">5. ANÁLISIS CLÍNICO</h1>
